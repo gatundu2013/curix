5dc10088a4f4be1cf1cdef52255786195d440a05
gatundu2013 <brianwgatundu@gmail.com>
2025-07-24 17:05:43 +0300

    refactor: Add database retry mechanism

--- backend/src/db/connection/postgress.ts
+++ backend/src/db/connection/postgress.ts
@@ -7,12 +7,25 @@ const pool = new Pool({ connectionString: envVariables.DATABASE_URL });
 
 export const db = drizzle({ client: pool });
 
-export async function connectDb() {
-  try {
-    await db.execute(`SELECT 1`);
-    console.log(`${icons.success} Database connected successfully:`);
-  } catch (err) {
-    console.error(`${icons.error} Database connection failed:`);
-    throw err;
+export async function connectDb(maxRetries = 5, delay = 1000) {
+  let attempt = 0;
+  while (attempt < maxRetries) {
+    try {
+      await db.execute(`SELECT 1`);
+      console.log(`${icons.success} Database connected successfully:`);
+      return;
+    } catch (err: any) {
+      attempt++;
+      console.error(
+        `${icons.error} Database connection failed (attempt ${attempt}):`,
+        err
+      );
+      if (attempt >= maxRetries) {
+        throw new Error("Max database connection attempts reached");
+      }
+
+      // Wait before retrying
+      await new Promise((res) => setTimeout(res, delay));
+    }
   }
 }
